bfea16e68d7e99bdcc4410709f3173193631aa71
ahmedyehia122s <143211370+ahmedyehia122s@users.noreply.github.com>
2023-08-25 20:44:57 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -22,7 +22,7 @@ class homepage1 extends StatelessWidget {
       body: Container(
         height: 200,
         width: 200,
-        margin: EdgeInsets.all(12),
+        margin: EdgeInsets.all(9),
         // padding: EdgeInsets.symmetric(horizontal: 50, vertical: 70),
         alignment: Alignment.center,
         decoration: BoxDecoration(
